95761d82904b6f5db6824d68753c939a07365ce8
Danila Miasniankin <danila_miasniankin@epam.com>
2020-06-24 18:33:03 +0300

    existing UI tests were refactored, comments were added

--- WeatherAppUITesting/BaseTest.cs
+++ WeatherAppUITesting/BaseTest.cs
@@ -1,4 +1,6 @@
 ﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
@@ -9,15 +11,28 @@ namespace WeatherAppUITesting
     public class BaseTest
     {
         protected Application WPFApp;
+        protected Window window;
+        protected Grid weatherDataGrid;
 
+        /// <summary>
+        /// This method launches app, initializes main window and weather grid
+        /// </summary>
         [TestInitialize]
         public void TestInitialize()
         {
             WPFApp = Application.Launch(@"..\..\..\WeatherAppWPF\bin\Debug\WeatherAppWPF.exe");
+            using (var automation = new UIA3Automation())
+            {
+                window = WPFApp.GetMainWindow(automation);
+                weatherDataGrid = window.FindFirstDescendant(element => element.ByAutomationId("WeatherGrid"))?.AsGrid();
+            }
         }
 
+        /// <summary>
+        /// This method closes app and deletes json with saved cities
+        /// </summary>
         [TestCleanup]
-        public void TestCleanup()
+        public void TestCleanup()//closing app and deleting 
         {
             WPFApp.Close();
             File.Delete($"{Environment.CurrentDirectory}\\WeatherApp.json");
--- WeatherAppUITesting/Tests.cs
+++ WeatherAppUITesting/Tests.cs
@@ -6,80 +6,75 @@ using FlaUI.Core.WindowsAPI;
 using FlaUI.UIA3;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FlaUI.Core;
-using FluentAssertions;
-using FluentAssertions.Common;
 
 namespace WeatherAppUITesting
 {
     [TestClass]
     public class Tests : BaseTest
     {
+        /// <summary>
+        /// This method checks that at the beginning list is empty
+        /// </summary>
         [TestMethod]
         public void CheckRowsCountNumber()
         {
-            using (var automation = new UIA3Automation())
-            {
-                var window = WPFApp.GetMainWindow(automation);
-                var weatherDataGrid = window.FindFirstDescendant(element => element.ByAutomationId("WeatherGrid"))?.AsGrid();
-                Assert.IsTrue(weatherDataGrid.RowCount == 1);
-            }
+            Assert.IsTrue(weatherDataGrid.RowCount == 1);
         }
 
+        /// <summary>
+        /// This method checks the ability to add and remove cities
+        /// </summary>
         [TestMethod]
         public void CheckAddingAndRemoving()
         {
-            using (var automation = new UIA3Automation())
-            {
-                var window = WPFApp.GetMainWindow(automation);
-                var weatherDataGrid = window.FindFirstDescendant(element => element.ByAutomationId("WeatherGrid"))?.AsGrid();
-                var firstCell = weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().AsGridCell();
-                firstCell.DoubleClick();
-                Keyboard.Press(VirtualKeyShort.KEY_K);
-                Keyboard.Press(VirtualKeyShort.KEY_H);
-                Keyboard.Press(VirtualKeyShort.KEY_A);
-                Keyboard.Press(VirtualKeyShort.KEY_R);
-                Keyboard.Press(VirtualKeyShort.KEY_K);
-                Keyboard.Press(VirtualKeyShort.KEY_I);
-                Keyboard.Press(VirtualKeyShort.KEY_V);
-                Keyboard.Press(VirtualKeyShort.ENTER);
-                Thread.Sleep(2000);
-                firstCell = weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().AsGridCell();
-                Assert.IsTrue(weatherDataGrid.RowCount == 2);
-                firstCell.Click();
-                Keyboard.Press(VirtualKeyShort.DELETE);
-                Thread.Sleep(2000);
-                Assert.IsTrue(weatherDataGrid.RowCount == 1);
-            }
+            var firstCell = weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().AsGridCell();
+            firstCell.DoubleClick();
+            Keyboard.Press(VirtualKeyShort.KEY_K);
+            Keyboard.Press(VirtualKeyShort.KEY_H);
+            Keyboard.Press(VirtualKeyShort.KEY_A);
+            Keyboard.Press(VirtualKeyShort.KEY_R);
+            Keyboard.Press(VirtualKeyShort.KEY_K);
+            Keyboard.Press(VirtualKeyShort.KEY_I);
+            Keyboard.Press(VirtualKeyShort.KEY_V);
+            Keyboard.Press(VirtualKeyShort.ENTER);
+            Thread.Sleep(2000);//waiting for correct input
+            firstCell = weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().AsGridCell();
+            Assert.IsTrue(weatherDataGrid.RowCount == 2);
+            firstCell.Click();
+            Keyboard.Press(VirtualKeyShort.DELETE);
+            Thread.Sleep(2000);//waiting for removal
+            Assert.IsTrue(weatherDataGrid.RowCount == 1);
         }
 
+        /// <summary>
+        /// This method checks the ability to save cities to the list
+        /// </summary>
         [TestMethod]
         public void CheckResultSaving()
         {
-            using (var automation = new UIA3Automation())
-            {
-                var window = WPFApp.GetMainWindow(automation);
-                var weatherDataGrid = window.FindFirstDescendant(element => element.ByAutomationId("WeatherGrid"))?.AsGrid();
-                var firstCell = weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().AsGridCell();
-                firstCell.DoubleClick();
-                Keyboard.Press(VirtualKeyShort.KEY_K);
-                Keyboard.Press(VirtualKeyShort.KEY_H);
-                Keyboard.Press(VirtualKeyShort.KEY_A);
-                Keyboard.Press(VirtualKeyShort.KEY_R);
-                Keyboard.Press(VirtualKeyShort.KEY_K);
-                Keyboard.Press(VirtualKeyShort.KEY_I);
-                Keyboard.Press(VirtualKeyShort.KEY_V);
-                Keyboard.Press(VirtualKeyShort.ENTER);
-                Thread.Sleep(2000);
-                Assert.IsTrue(weatherDataGrid.RowCount == 2);
-                Assert.IsTrue(weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().Value.Equals("kharkiv"));
-            }
+            var firstCell = weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().AsGridCell();
+            firstCell.DoubleClick();
+
+            Keyboard.Press(VirtualKeyShort.KEY_K);
+            Keyboard.Press(VirtualKeyShort.KEY_H);
+            Keyboard.Press(VirtualKeyShort.KEY_A);
+            Keyboard.Press(VirtualKeyShort.KEY_R);
+            Keyboard.Press(VirtualKeyShort.KEY_K);
+            Keyboard.Press(VirtualKeyShort.KEY_I);
+            Keyboard.Press(VirtualKeyShort.KEY_V);
+            Keyboard.Press(VirtualKeyShort.ENTER);
+            Thread.Sleep(2000);//waiting for correct input
+
+            Assert.IsTrue(weatherDataGrid.RowCount == 2);
+            Assert.IsTrue(weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().Value.Equals("kharkiv"));
+
             WPFApp.Close();
             WPFApp = Application.Launch(@"..\..\..\WeatherAppWPF\bin\Debug\WeatherAppWPF.exe");
             using (var automation = new UIA3Automation())
             {
-                var window = WPFApp.GetMainWindow(automation);
-                var weatherDataGrid = window.FindFirstDescendant(element => element.ByAutomationId("WeatherGrid"))?.AsGrid();
-                var firstCell = weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().AsGridCell();
+                window = WPFApp.GetMainWindow(automation);
+                weatherDataGrid = window.FindFirstDescendant(element => element.ByAutomationId("WeatherGrid"))?.AsGrid();
+                firstCell = weatherDataGrid.Rows.FirstOrDefault().Cells.FirstOrDefault().AsGridCell();
                 var secondCell = weatherDataGrid.Rows.FirstOrDefault().Cells.Last().AsGridCell();
                 Assert.IsTrue(firstCell.Value.Equals("kharkiv"));
                 Assert.IsFalse(secondCell.Equals(string.Empty));
